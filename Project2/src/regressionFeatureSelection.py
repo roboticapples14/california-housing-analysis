6516ead9f509849cc199edb572aa041f93ca83e6
Jordan Shopp <jordan.shopp123@gmail.com>
2023-03-19 20:10:33 +0100

    Added a multi-feature selection script

---
+++ Project2/src/regressionFeatureSelection.py
@@ -0,0 +1,171 @@
+# exercise 6.2.1
+from matplotlib.pyplot import figure, plot, subplot, title, xlabel, ylabel, show, clim
+from scipy.io import loadmat
+import sklearn.linear_model as lm
+from sklearn import model_selection
+from toolbox_02450 import feature_selector_lr, bmplot
+import numpy as np
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+import os
+import sklearn.linear_model as lm
+from matplotlib.pylab import figure, subplot, plot, xlabel, ylabel, hist, show
+
+# Load data from matlab file
+DATA_PATH = '../../data'
+housing_df_raw = pd.read_csv(os.path.join(DATA_PATH, 'housing.csv'))
+housing_df = housing_df_raw.dropna()
+
+housing_df_numeric = housing_df.iloc[:, :housing_df.shape[1] - 1]
+
+# m = entries, n = attributes
+N, M = housing_df_numeric.shape
+
+# normalize data
+#df_normalized=(housing_df_numeric - housing_df_numeric.mean()) / housing_df_numeric.std()
+X = housing_df_numeric.values
+N, M = X.shape
+med_inc_index = 7
+y = X[:,med_inc_index]
+X_cols = list(range(0,med_inc_index)) + list(range(med_inc_index+1,M))
+X = X[:,X_cols]
+N, M = X.shape
+
+attributeNames= ['Long', 'Lat', 'Med Age', 'Rooms', 'Bedrooms', 'Pop', 'Households', 'MedHouseValue']
+
+
+
+#add more attributes, first, division
+col = 0
+for i in range(M):
+    for j in range(M):
+        if i!=j:
+            addition = (X[:,i]/X[:,j]).reshape(-1,1)
+            X = np.asarray(np.bmat('X, addition'))
+            attributeNames.append(attributeNames[i]+'/'+attributeNames[j])
+for i in range(M):
+    for j in range(M):
+        addition = (X[:,i]*X[:,j]).reshape(-1,1)
+        X = np.asarray(np.bmat('X, addition'))
+        attributeNames.append(attributeNames[i]+'*'+attributeNames[j])
+        
+
+N, M = X.shape
+
+
+
+## Crossvalidation
+# Create crossvalidation partition for evaluation
+K = 5
+CV = model_selection.KFold(n_splits=K,shuffle=True)
+
+# Initialize variables
+Features = np.zeros((M,K))
+Error_train = np.empty((K,1))
+Error_test = np.empty((K,1))
+Error_train_fs = np.empty((K,1))
+Error_test_fs = np.empty((K,1))
+Error_train_nofeatures = np.empty((K,1))
+Error_test_nofeatures = np.empty((K,1))
+
+k=0
+for train_index, test_index in CV.split(X):
+    
+    # extract training and test set for current CV fold
+    X_train = X[train_index,:]
+    y_train = y[train_index]
+    X_test = X[test_index,:]
+    y_test = y[test_index]
+    internal_cross_validation = 10
+    
+    # Compute squared error without using the input data at all
+    Error_train_nofeatures[k] = np.square(y_train-y_train.mean()).sum()/y_train.shape[0]
+    Error_test_nofeatures[k] = np.square(y_test-y_test.mean()).sum()/y_test.shape[0]
+
+    # Compute squared error with all features selected (no feature selection)
+    m = lm.LinearRegression(fit_intercept=False).fit(X_train, y_train)
+    Error_train[k] = np.square(y_train-m.predict(X_train)).sum()/y_train.shape[0]
+    Error_test[k] = np.square(y_test-m.predict(X_test)).sum()/y_test.shape[0]
+
+    # Compute squared error with feature subset selection
+    textout = ''
+    selected_features, features_record, loss_record = feature_selector_lr(X_train, y_train, internal_cross_validation,display=textout)
+    
+    Features[selected_features,k] = 1
+    # .. alternatively you could use module sklearn.feature_selection
+    if len(selected_features) == 0:
+        print('No features were selected, i.e. the data (X) in the fold cannot describe the outcomes (y).' )
+    else:
+        m = lm.LinearRegression(fit_intercept=True).fit(X_train[:,selected_features], y_train)
+        Error_train_fs[k] = np.square(y_train-m.predict(X_train[:,selected_features])).sum()/y_train.shape[0]
+        Error_test_fs[k] = np.square(y_test-m.predict(X_test[:,selected_features])).sum()/y_test.shape[0]
+    
+        figure(k,figsize=(100,50))
+        subplot(1,2,1)
+        plot(range(1,len(loss_record)), loss_record[1:])
+        xlabel('Iteration')
+        ylabel('Squared error (crossvalidation)')    
+        
+        subplot(1,3,3)
+        bmplot(attributeNames, range(1,features_record.shape[1]), -features_record[:,1:])
+        clim(-1.5,0)
+        xlabel('Iteration')
+
+    print('Cross validation fold {0}/{1}'.format(k+1,K))
+    print('Train indices: {0}'.format(train_index))
+    print('Test indices: {0}'.format(test_index))
+    print('Features no: {0}\n'.format(selected_features.size))
+
+    k+=1
+
+
+# Display results
+print('\n')
+print('Linear regression without feature selection:\n')
+print('- Training error: {0}'.format(Error_train.mean()))
+print('- Test error:     {0}'.format(Error_test.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test.sum())/Error_test_nofeatures.sum()))
+print('Linear regression with feature selection:\n')
+print('- Training error: {0}'.format(Error_train_fs.mean()))
+print('- Test error:     {0}'.format(Error_test_fs.mean()))
+print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_fs.sum())/Error_train_nofeatures.sum()))
+print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_fs.sum())/Error_test_nofeatures.sum()))
+
+figure(k, figsize=(100,50))
+subplot(1,3,2)
+bmplot(attributeNames, range(1,Features.shape[1]+1), -Features)
+clim(-1.5,0)
+xlabel('Crossvalidation fold')
+ylabel('Attribute')
+
+
+# Inspect selected feature coefficients effect on the entire dataset and
+# plot the fitted model residual error as function of each attribute to
+# inspect for systematic structure in the residual
+
+f=2 # cross-validation fold to inspect
+ff=Features[:,f-1].nonzero()[0]
+if len(ff) == 0:
+    print('\nNo features were selected, i.e. the data (X) in the fold cannot describe the outcomes (y).' )
+else:
+    m = lm.LinearRegression(fit_intercept=True).fit(X[:,ff], y)
+    
+    y_est= m.predict(X[:,ff])
+    residual=y-y_est
+    
+    figure(k+1, figsize=(30,20))
+    title('Residual error vs. Attributes for features selected in cross-validation fold {0}'.format(f))
+    for i in range(0,len(ff)):
+       subplot(2, int( np.ceil(len(ff) / 2.0)),i+1)
+       plot(X[:,ff[i]],residual,'.')
+       xlabel(attributeNames[ff[i]])
+       ylabel('residual error')
+    
+    
+show()
+print(attributeNames)
+print('Ran Exercise 6.2.1')
